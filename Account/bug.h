5b55048b4a644f5ff330081cde14a456b8abe501
zhanghong <zhanghong@CY-MO-zhanghong.local>
2016-02-16 15:17:57 +0800

    新增bug文件

---
+++ Account/bug.h
@@ -0,0 +1,13 @@
+//
+//  bug.h
+//  Account
+//
+//  Created by zhanghong on 16/2/16.
+//  Copyright © 2016年 zhanghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bug : UIView
+
+@end
---
+++ Account/bug.m
@@ -0,0 +1,14 @@
+
+//
+//  bug.m
+//  Account
+//
+//  Created by zhanghong on 16/2/16.
+//  Copyright © 2016年 zhanghong. All rights reserved.
+//
+
+#import "bug.h"
+
+@implementation bug
+
+@end
